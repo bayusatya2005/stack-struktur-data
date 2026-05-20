5c30fb95091192a753127495ec6142bbf4bdf415
bayusatya2005 <bayusagara190525@gmail.com>
2026-05-20 12:28:00 +0800

    Add files via upload

---
+++ Tugas3 (1).py
@@ -0,0 +1,11 @@
+
+angka = int(input("Masukkan Angka:: "))
+
+if angka < 0:
+    print("Angka Harus Positif!")
+
+else:
+    ("Selesai")
+
+print("Angka yang dimasukkan: ", angka)
+print("Program Selesai")
